4a86859093663700ce531c88ec38c1a8a8cf289b
Benjamin Badger <54602201+blbadger@users.noreply.github.com>
2020-12-24 23:31:03 -0500

    Update newton_method_incremented.py

--- newton_method_incremented.py
+++ newton_method_incremented.py
@@ -172,7 +172,8 @@ def evaluate(equation, point):
 
 def newton_raphson_map(equation, max_iterations, x_range, y_range, t):
 	print (equation)
-	y, x = np.ogrid[-0.7: 0.7: y_range*1j, -1: 1: x_range*1j]
+	# top left to bottom right
+	y, x = np.ogrid[0.7: -0.7: y_range*1j, -1: 1: x_range*1j]
 	z_array = x + y*1j
 
 	iterations_until_rooted = max_iterations + np.zeros(z_array.shape)
